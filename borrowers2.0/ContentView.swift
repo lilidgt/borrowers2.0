2e2633afb622cfb47c8fd410f8a109fa290dc0a8
Lissa Deguti <lilideguti@gmail.com>
2025-08-01 08:35:30 -0300

    Initial Commit

---
+++ borrowers2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  borrowers2.0
+//
+//  Created by Lissa Deguti on 01/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
